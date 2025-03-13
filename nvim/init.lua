b5ee23a1c3db79bd1c745aec859848ddbd0f1938
Sergiu Albu <sergiu.albu@snpglobal.com>
2025-03-13 09:19:51 +0200

    Added new config for nvim and tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,42 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.autocommands"
-require "user.colorscheme"
-require "user.colorizer"
-require "user.cmp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.illuminate"
-require "user.indentline"
-require "user.alpha"
-require "user.lsp"
-require "user.dap"
+-- Declare the path where lazy will clone plugin code
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- Check to see if lazy itself has been cloned, if not clone it into the lazy.nvim directory
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+-- Add the path to the lazy plugin repositories to the vim runtime path
+vim.opt.rtp:prepend(lazypath)
+
+-- Declare a few options for lazy
+local opts = {
+	change_detection = {
+		-- Don't notify us every time a change is made to the configuration
+		notify = false,
+	},
+	checker = {
+		-- Automatically check for package updates
+		enabled = true,
+		-- Don't spam us with notification every time there is an update available
+		notify = false,
+	},
+}
+
+-- Load the options from the config/options.lua file
+require("config.options")
+-- Load the keymaps from the config/keymaps.lua file
+require("config.keymaps")
+-- Load the auto commands from the config/autocmds.lua file
+require("config.autocmds")
+-- Setup lazy, this should always be last
+-- Tell lazy that all plugin specs are found in the plugins directory
+-- Pass it the options we specified above
+require("lazy").setup("plugins", opts)
---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Setup our JDTLS server any time we open up a java file
+vim.cmd [[
+    augroup jdtls_lsp
+        autocmd!
+        autocmd FileType java lua require'config.jdtls'.setup_jdtls()
+    augroup end
+]]
+
+-- Setup Terraform
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+
